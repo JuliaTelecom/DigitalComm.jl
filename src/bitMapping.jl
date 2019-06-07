3267fccde1e986e3b1930dec29ffe7b946960702
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2019-06-07 14:55:23 +0200

    Correcting a bug in string MD method call

--- src/bitMapping.jl
+++ src/bitMapping.jl
@@ -181,7 +181,7 @@ end
 function bitMappingQAM!(qamMat,M::String, bitSeq)
 	# --- Casting modulation order to int8
 	if M == "QPSK" || M == "4-QAM" || M == "QAM-4" || M=="4QAM" || M == "QAM4"
-		bitMappingQAM!(qamMat,,bitSeq);
+		bitMappingQAM!(qamMat,4,bitSeq);
 	elseif M == "16-QAM" || M == "QAM-16" || M=="16QAM" || M == "QAM16"
 		bitMappingQAM!(qamMat,16,bitSeq);
 	elseif M == "64-QAM" || M == "QAM-64" || M=="64QAM" || M == "QAM64"
