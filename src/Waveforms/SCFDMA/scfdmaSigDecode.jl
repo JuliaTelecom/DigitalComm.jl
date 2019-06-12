4df21c320c9b7e928a08f7145fef9c3be35bd1b2
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2019-06-12 11:35:19 +0200

    Adding SC-FDMA Tx Rx functions

---
+++ src/Waveforms/SCFDMA/scfdmaSigDecode.jl
@@ -0,0 +1,94 @@
+""" scfdmaSigDecode
+---  
+Apply SCFDMA demodulator to input signal and returns the T/F QAM matrix
+# --- Syntax 
+qamRx	= scfdmaSigDecode(sigId,nFFT,nCP,allocSubcarriers,sizeDFT);
+# --- Input parameters 
+# ---  Input parameters
+- sigId	  : SCFDMA signal in time domain [Array{Complex{Float64},nbEch}]
+- nFFT	  : FFT size [Int]
+- nCp	  : Cyclic prefix size (in samples) [Int]
+- allocSubcarriers : Vector of index of allocated subcarriers [Array{Int,nbSubcarriers}]
+- sizeDFT	: Post processing size (DFT size)
+- doPostProcessing : Do the post processing stage (IDFT): Default 1. In some case, we need the raw data (i.e data before postprocessing stage) such as in channel equalisation. In this case, the post processing should be done in a separate step (with the call of scfdmaPostProcessing function)
+# ---  Output parameters
+- qamRx  : Time frequency matrix : [Array{Complex{Float64},nbSubcarriers,nbSymb}]
+	  * nbSymb			: Number of SCFDMA symbol tro be transmitted
+	  * nbSubcarriers	: Number of allocated subcarriers (shall be < nFFT)
+# --- 
+# v 1.0 - Robin Gerzaguet.
+"""
+function scfdmaSigDecode(sigChan,nFFT,nCP,allocSubcarriers,sizeDFT,doPostProcessing=1)
+	# Set 4 core for FFT computation
+	FFTW.set_num_threads(4)
+	# --- Getting symbol size
+	sizeSymb	   = nFFT + nCP;
+	# --- Getting number of symbol to decode
+	nbSymbDecode   = Int(floor(length(sigChan)/sizeSymb));
+	# --- Initiate Rx T/F matrix
+	qamRx		   = zeros(Complex{Float64},length(allocSubcarriers),nbSymbDecode);
+	# --- SCFDMA demodulator
+	for n = 1  : 1 : nbSymbDecode
+		# --- Getting current symbol without CP
+		currSymb  = sigChan[sizeSymb*(n-1).+collect(1+nCP:sizeSymb)];
+		# --- TF
+		fftEl	  = fft(currSymb,1);
+		# --- Mapping in output matrix
+		qamRx[:,n]		= fftEl[allocSubcarriers];
+	end
+	if Bool(doPostProcessing)
+		qamRx	= scfdmaPostProcessing(qamRx,sizeDFT);
+	end
+	return qamRx;
+end
+
+""" scfdmaPostProcessing
+---  
+Apply Post processing for SC-FDMA (i.e DFT post-processing stage). This function can be called if scfdmaSigDecode is called with post processing flag to 0 (if frequency egalisation is done for example)
+# --- Syntax 
+qamPost	  = scfdmaPostProcessing(qamMat,sizeDFT)
+# --- Input parameters 
+- qamMat  : T/F matrix after Rx FFT (and before Rx IDFT) [Array{Complex{Float64}},nbSubcarriers,nbSymb]
+- sizeDFT	: Precoder bloc size (often 12)
+# --- Output parameters 
+- qamPost	: T/F matrix after IDFT processing [Array{Complex{Float64},nbSubcarriers,nbSymb}]
+	* nbSubcarriers : Number of allocated subcarriers 
+	* nbSymb		: Numbber of decoded symbols
+# --- 
+# v 1.0 - Robin Gerzaguet.
+"""
+function scfdmaPostProcessing(qamMat,sizeDFT)
+	nbSymb		= size(qamMat,2);
+	nbSubcarriers = size(qamMat,1);
+	nbCarriers	= nbSubcarriers ÷ sizeDFT;
+	qamIDFT		= zeros(Complex{Float64},nbSubcarriers,nbSymb);
+	for iN = 1 : 1 : nbSymb 
+		for iC = 1 : 1 : nbCarriers
+			# --- Apply DFT to current carrier, at current symbol
+			qamIDFT[(iC-1)*sizeDFT .+ (1:sizeDFT),iN] = ifft(qamMat[(iC-1)*sizeDFT  .+ (1:sizeDFT),iN]);
+		end 
+	end
+	return qamIDFT
+end
+
+
+""" scfdmaSigDecode
+---  
+Apply SCFDMA demodulator to input signal and returns the T/F QAM matrix
+# --- Syntax 
+qamRx	= scfdmaSigDecode(sigId,nFFT,nCP,allocSubcarriers,sizeDFT);
+# --- Input parameters 
+# ---  Input parameters
+- sigId	  : SCFDMA signal in time domain [Array{Complex{Float64},nbEch}]
+- scfdma  : SCFDMA structure [StrucSCFDMA]
+- doPostProcessing : Do the post processing stage (IDFT): Default 1. In some case, we need the raw data (i.e data before postprocessing stage) such as in channel equalisation. In this case, the post processing should be done in a separate step (with the call of scfdmaPostProcessing function)
+# ---  Output parameters
+- qamRx  : Time frequency matrix : [Array{Complex{Float64},nbSubcarriers,nbSymb}]
+	  * nbSymb			: Number of SCFDMA symbol tro be transmitted
+	  * nbSubcarriers	: Number of allocated subcarriers (shall be < nFFT)
+# --- 
+# v 1.0 - Robin Gerzaguet.
+"""
+function scfdmaSigDecode(sigChan,scfdma::StrucSCFDMA,doPostProcessing=1)
+	return scfdmaSigDecode(sigChan,scfdma.nFFT,scfdma.nCP,scfdma.allocatedSubcarriers,sizeDFT,doPostProcessing);
+end
---
+++ src/Waveforms/SCFDMA/scfdmaSigGen.jl
@@ -0,0 +1,105 @@
+""" StrucSCFDMA
+---  
+Structure for SCFDMA
+# --- Syntax 
+- nFFT		: FFT size [Int] 
+- nCP		: Cyclic prefix size [Int] 
+- allocatedSubcarriers	: Vector of allocated subbcarriers [Array{Int}]
+- sizeDFT	: Size of DFT precoder (classic value is 12) [Int]
+# --- 
+# v 1.0 - Robin Gerzaguet.
+"""
+struct StrucSCFDMA<: Waveform
+	nFFT::Int;
+	nCP::Int;
+	allocatedSubcarriers::Array{Int};
+	sizeDFT::Int;
+end
+
+
+""" initSCFDMA
+---  
+Create SCFDMA structure 
+# --- Syntax 
+ofdm	= initSCFDMA(nFFT,nCP,allocatedSubcarriers)
+# --- Input parameters 
+- nFFT	  : FFT size [Int]
+- nCP	  : Cyclic prefix size [Int]
+- allocatedSubcarrier	: Vector of allocated subbcarriers [Array{Int}]
+- sizeDFT	: Size of DFT precoder (classic value is 12) [Int]
+# --- Output parameters 
+- scfdma: SCFDMA structure [StrucSCFDMA] 
+# --- 
+# v 1.0 - Robin Gerzaguet.
+"""
+function initSCFDMA(nFFT,nCP,allocatedSubcarriers,sizeDFT)
+	# ---Checking FFT size
+	if maximum(allocatedSubcarriers) > nFFT
+		error("Subcarrier allocation is impossible");
+	end
+	# --- Create the SCFDMA structure
+	return StrucSCFDMA(nFFT,nCP,allocatedSubcarriers,sizeDFT)
+end
+
+""" scfdmaSigGen
+---  
+Create SCFDMA signal in time domain based on input T/F matrix and SCFDMA parameters. SCFDMA apply a precoder before the IFFT at the transmitter side in order to lower the signal fluctuation
+qamMat is a complex symbol matrix (for instance QPSK) of size length(allocatedSubcarriers) x nbSymb 
+The output signal in time domain is of size (nFFT+nCP)xnbSymb.
+# --- Syntax 
+  sigId = scfdmaSigGen(qamMat,nFFT,nCP,allocatedSubcarriers)
+# --- Input parameters 
+- qamMat  : Complex T/F matrix to map [Array{Float64},length(allocatedSubcarriers),nbSymb]
+- nFFT	  : FFT size [Int] 
+- nCP	  : Cylic prefix size [Int] 
+- allocatedSubcarrier	: Vector of allocated subbcarriers [Array{Int}]
+- sizeDFT	: Size of DFT precoder (classic value is 12) [Int]
+# --- Output parameters 
+- sigId	  : Signal in time domain [Array{Complex{Float64}},(nFFT+nCP)xnbSymb]
+# --- 
+# v 1.0 - Robin Gerzaguet.
+"""
+function scfdmaSigGen(qamMat,nFFT,nCP,allocatedSubcarriers,sizeDFT)
+	# Set 4 core for FFT computation
+	FFTW.set_num_threads(4)
+	# --- Getting parameters
+	nbSymb		= size(qamMat,2); 		# --- Applied on x symbols
+	symbSize	= nFFT + nCP; 			# --- Symbol size
+	nSize 		= symbSize*nbSymb; 		# --- Total burst length
+	# --- DFT pre-processing  
+	nbCarriers	= length(allocatedSubcarriers)÷sizeDFT;
+	qamDFT		= zeros(Complex{Float64},length(allocatedSubcarriers),nbSymb);
+	for iN = 1 : 1 : nbSymb 
+		for iC = 1 : 1 : nbCarriers
+			# --- Apply DFT to current carrier, at current symbol
+			qamDFT[(iC-1)*sizeDFT .+ (1:sizeDFT),iN] = fft(qamMat[(iC-1)*sizeDFT  .+ (1:sizeDFT),iN]);
+		end 
+	end
+	# --- Mapping to nFFT elements
+	qamCurr		= zeros(Complex{Float64},nFFT,nbSymb);
+	qamCurr[allocatedSubcarriers,:] = qamDFT;
+	# --- Switch to time domain
+	sTmp 	= ifft(qamCurr,1);
+	# --- Inserting Cyclic prefix
+	sig	  = [sTmp[end+1-nCP:end,:] ; sTmp][:];
+end
+
+""" scfdmaSigGen
+---  
+Create SCFDMA signal in time domain based on input T/F matrix and SCFDMA parameters. SCFDMA apply a precoder before the IFFT at the transmitter side in order to lower the signal fluctuation
+qamMat is a complex symbol matrix (for instance QPSK) of size length(allocatedSubcarriers) x nbSymb 
+The output signal in time domain is of size (nFFT+nCP)xnbSymb.
+# --- Syntax 
+sigId = scfdmaSigGen(qamMat,scfdma)
+# --- Input parameters 
+- qamMat  : Complex T/F matrix to map [Array{Float64},length(allocatedSubcarriers),nbSymb]
+- scfdma  : SC-FDMA structure [StrucSCFDMA]
+# --- Output parameters 
+- sigId	  : Signal in time domain [Array{Complex{Float64}},(nFFT+nCP)xnbSymb]
+# --- 
+# v 1.0 - Robin Gerzaguet.
+"""
+# --- MD is waveform structure is given
+function scfdmaSigGen(qamMat,scfdma::StrucSCFDMA)
+	return scfdmaSigGen(qamMat,scfdma.nFFT,scfdma.nCP,scfdma.allocatedSubcarriers,scfdma.sizeDFT);
+end
