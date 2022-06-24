36c9adb6f7c0c8b4fc0752ba8db325cfd24d9fcc
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2022-06-24 15:21:04 +0200

    Clarify doc in bang methods

--- src/Channel/addNoise.jl
+++ src/Channel/addNoise.jl
@@ -1,17 +1,15 @@
 """ 
----  
  Add a white additive circular gaussian noise to input signal
 Added noise is real if input signal is real and computedlex is input signal is complex
 Noise level is controled by the second input parameter which is the signal to noise ratio (SNR)
 Signal power powSig is computed based in the input sequence power (average power in time domain)
 although a third parameter (theoretical power of input signal is given). In that case, snr is computed
-based on the value powSig
-output parameters are the signal with noise, and the noise samples
-See the bang methods for non-buffer alloc.
-# --- Syntax 
+based on the value powSig \\
+output parameters are the signal with noise, and the noise samples \\
+See the bang methods for non-buffer alloc. \\
 [y,n]   = addNoise(x,snr,powSig);
-addNoise!(y,x,powSig)
-addNoise!(x,powSig)
+addNoise!(y,x,powSig)       # Fill y from x 
+addNoise!(x,powSig)         # Replace x
 # --- Input parameters 
 - x= Input signal [Array{Real{Float64}}, Array{Complex{Float64}}] of size N
 - snr= Desired signal to noise ratio [Float64]
